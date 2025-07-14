ab7d374f833bfefa4c621274f2825c46c901bace
ajaz <ajaz2468@gmail.com>
2025-07-14 14:16:06 +0400

    Update next.config.ts and package.json for deployment setup and new dependencies

--- intellident-admin-app/next.config.ts
+++ intellident-admin-app/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: "/Intellident_admin_hub",
+  assetPrefix: "/Intellident_admin_hub/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
